3e5ed43fb3c969d6b6b8ef5c24d091e8a8545d46
blazej <blawozn@gmail.com>
2024-01-04 13:53:49 +0100

    First commit.

---
+++ src/main/java/com/example/githubapiintegrationhomework/GitHubApiIntegrationHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubapiintegrationhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubApiIntegrationHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubApiIntegrationHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubapiintegrationhomework/GitHubApiIntegrationHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubapiintegrationhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubApiIntegrationHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
